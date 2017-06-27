37a2183efc29ee0ed35866acad34dd0a06c34f7f
J-C <jcjoneskern@gmail.com>
2017-06-27 15:47:43 -0400

    a bit better but still not working quite right

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 $(function() {
 
   let index = 4;
+  let elems = ["one", "two", "three"];
+  let hold = 0;
 
   $("li").on("click", function() {
-    let panel = $("div[data-panel=" + $(this).attr("data-panel") + "]");
+    let nav = $(this).attr("data-panel");
+    let panel = $("div[data-panel=" + nav + "]");
 
     if (panel.hasClass("visible")) {
       return;
     } else if (panel.hasClass("hidden")) {
-      index--;
-      $("div.visible").toggleClass("hidden visible").css("z-index", index);
+      if (elems.indexOf(nav) > hold) {
+        //slide in from right
+        index--;
+        $("div.visible").toggleClass("hidden visible").css("z-index", index).css("left",0);
 
-      index++;
-      panel.toggleClass("hidden visible").css("z-index", index);
+        index+=2;
+        panel.toggleClass("hidden visible").css("z-index", index).css("right", 0);
+      } else {
+        //slide in from left
+        index--;
+        $("div.visible").toggleClass("hidden visible").css("z-index", index).css("right",0);
+
+        index+=2;
+        panel.toggleClass("hidden visible").css("z-index", index).css("left", 0);
+      }
+
+      hold = elems.indexOf(nav);
     }
 
   });
