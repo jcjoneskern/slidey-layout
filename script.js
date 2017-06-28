65b041e6d91d04128c10779be421e6c7856083aa
J-C <jcjoneskern@gmail.com>
2017-06-27 22:12:51 -0400

    better?

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 $(function() {
 
   let index = 4;
-  let elems = ["one", "two", "three"];
+  let elems = ["home", "about", "portfolio", "contact"];
   let hold = 0;
 
   $("li").on("click", function() {
-    let nav = $(this).attr("data-panel");
-    let panel = $("div[data-panel=" + nav + "]");
+    let nav = $(this).text();
+    let panel = $("section#" + nav);
 
     if (panel.hasClass("show-left") || panel.hasClass("show-right")) {
       return;
     } else {
       if (elems.indexOf(nav) > hold) {
         index--;
-        $("div.show-left, div.show-right").attr("class", "hide-left").css("z-index", index);
+        $("section.show-left, section.show-right").attr("class", "hide-left").css("z-index", index);
 
         index += 2;
         panel.attr("class", "show-right").css("z-index", index).css("z-index", index);
       } else {
         index--;
-        $("div.show-left, div.show-right").attr("class", "hide-right").css("z-index", index);
+        $("section.show-left, section.show-right").attr("class", "hide-right").css("z-index", index);
 
         index +=2;
         panel.attr("class", "show-left").css("z-index", index);
